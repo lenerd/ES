c001f76495aedea7d2b048d72f92a9f73b23d7d1
Lennart Braun <3braun@tams121.informatik.uni-hamburg.de>
2015-04-17 12:43:19 +0200

    grrr

--- blatt_02/aufgabe_2-1/aufgabe_2-1.ino
+++ blatt_02/aufgabe_2-1/aufgabe_2-1.ino
@@ -2,48 +2,99 @@ const int led_pin = 7;
 const int t1_pin = 5;
 const int t2_pin = 3;
 
-uint8_t brightness = 0;
+volatile uint8_t t_cnt = 0;
+volatile uint8_t t_id = 0;
+volatile uint8_t brightness = 100;
+volatile bool next_state = LOW;
 
-void t1_int ()
-{
-    brightness += 5;
-}
+void TC6_Handler (void)
+{ 
+    uint32_t status;
+    status = TC2->TC_CHANNEL[0].TC_SR;
 
-void t2_int ()
-{
-    brightness -= 5;
+    // ++t_cnt;
+    // if (t_cnt > 1)
+    // {
+    //     if (next_state == HIGH)
+    //     {
+    //         brightness = 255;
+    //         next_state = LOW;
+    //         t_cnt = 0;
+    //     }
+    //     else
+    //     {
+    //         brightness = 0;
+    //         next_state = HIGH;
+    //         t_cnt = 0;
+    //     }
+    // }
+    // Serial.println("foo");
+    // return;
+
+    if (digitalRead(t1_pin) == LOW)
+    {
+        if (t_id != 1)
+        {
+            t_id = 1;
+            t_cnt = 0;
+        }
+        ++t_cnt;
+        if (t_cnt >= 32)
+        {
+            brightness += 5;
+            t_cnt = 0;
+        }
+    }
+    else if (digitalRead(t2_pin) == LOW)
+    {
+        if (t_id != 2)
+        {
+            t_id = 2;
+            t_cnt = 0;
+        }
+        ++t_cnt;
+        if (t_cnt >= 32)
+        {
+            brightness -= 5;
+            t_cnt = 0;
+        }
+    }
+    else
+    {
+        t_cnt = 0;
+    }
 }
 
 void setup()
 {
+
     uint32_t cmr_val = 0;
     pmc_set_writeprotect(false);
-    pmc_enable_periph_clk(ID_TC0);
+    pmc_enable_periph_clk(ID_TC6);
 
     cmr_val |= TC_CMR_WAVE;         /* waveform mode */
     cmr_val |= TC_CMR_WAVSEL_UP_RC; /* counting up until eq RC */
-    /* TODO: clock selection */
-    /* TODO: set RC val */
-    TC_Configure(TC0, 0, cmr_val);
-    TC0->TC_CHANNEL[0].TC_IER = TC_IER_CPCS; /* enable rc cmp interrupt */
-    /* TODO: isr */
+    cmr_val |= TC_CMR_TCCLKS_TIMER_CLOCK3; /* MCK/32 */
+    TC2->TC_CHANNEL[0].TC_RC = 2625; /* set RC val */
+    TC_Configure(TC2, 0, cmr_val);
+    TC2->TC_CHANNEL[0].TC_IER = TC_IER_CPCS; /* enable rc cmp interrupt */
+    TC2->TC_CHANNEL[0].TC_IDR = ~TC_IER_CPCS; /* not disable rc cmp interrupt */
 
-    NVIC_ClearPendingIRQ(TC0_IRQn);
-    NVIC_EnableIRQ(TC0_IRQn);
-
-    TC_Start(TC0, 0);
-
+    NVIC_ClearPendingIRQ(TC6_IRQn);
+    NVIC_EnableIRQ(TC6_IRQn);
 
     pinMode(led_pin, OUTPUT);
-    attachInterrupt(t1_pin, &t1_int, FALLING);
-    attachInterrupt(t2_pin, &t2_int, FALLING);
+    pinMode(t1_pin, INPUT);
+    pinMode(t2_pin, INPUT);
+    TC_Start(TC2, 0);
     Serial.begin(9600);
+    Serial.println("blubb\n");
+    // Serial.println("blubb\n");
 }
 
 void loop()
 {
+    delay(100);
     analogWrite(led_pin, brightness);
     Serial.println(brightness);
-    
-    delay(100);
 }
