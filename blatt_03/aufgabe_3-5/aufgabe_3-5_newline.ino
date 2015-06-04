5b04b239788c460ef280dbfd73d071b42fe1bb9a
Lennart Braun <lenerd@posteo.de>
2015-06-04 16:17:41 +0200

    more fancy automaton

---
+++ blatt_03/aufgabe_3-5/aufgabe_3-5_newline.ino
@@ -0,0 +1,159 @@
+#include <Servo.h>
+
+/* pins */
+const uint8_t servo_pin = 9;
+
+Servo servo;
+uint32_t angle = 90;
+const uint8_t min_angle = 25;
+const uint8_t max_angle = 155;
+
+int8_t bite;
+int8_t state;
+
+
+
+void setup()
+{
+    /* configure servo */
+    pinMode(servo_pin, OUTPUT);
+    servo.attach(servo_pin);
+    servo.write(90);
+
+    Serial.begin(9600);
+    Serial.println("setup done");
+    angle = 0;
+}
+
+void error (void)
+{
+    Serial.println("Use a command of the following form 'moveTo(\\d{1,3})'");
+    while (bite != '\n' && bite != -1)
+        bite = Serial.read();
+    state = 0;
+}
+
+void loop()
+{
+    while ((bite = Serial.read()) != -1)
+    {
+        switch(state)
+        {
+            case 0:
+                angle = 0;
+                if (bite == 'm')
+                    state = 1;
+                else
+                    error();
+                break;
+
+            case 1:
+                if (bite == 'o')
+                    state = 2;
+                else
+                    error();
+                break;
+
+            case 2:
+                if (bite == 'v')
+                    state = 3;
+                else
+                    error();
+                break;
+
+            case 3:
+                if (bite == 'e')
+                    state = 4;
+                else
+                    error();
+                break;
+
+            case 4:
+                if (bite == 'T')
+                    state = 5;
+                else
+                    error();
+                break;
+
+            case 5:
+                if (bite == 'o')
+                    state = 6;
+                else
+                    error();
+                break;
+
+            case 6:
+                if (bite == '(')
+                    state = 7;
+                else
+                    error();
+                break;
+
+            case 7:
+                if (bite >= '0' && bite <= '9')
+                {
+                    angle = bite - '0';
+                    state = 8;
+                }
+                else
+                    error();
+                break;
+
+            case 8:
+                if (bite >= '0' && bite <= '9')
+                {
+                    angle = angle * 10 + bite - '0';
+                    state = 9;
+                }
+                else if (bite == ')')
+                {
+                    state = 11;
+                }
+                else
+                    error();
+                break;
+
+            case 9:
+                if (bite >= '0' && bite <= '9')
+                {
+                    angle = angle * 10 + bite - '0';
+                    state = 10;
+                }
+                else if (bite == ')')
+                {
+                    state = 11;
+                }
+                else
+                    error();
+                break;
+
+            case 10:
+                if (bite == ')')
+                {
+                    state = 11;
+                }
+                else
+                    error();
+                break;
+
+            case 11:
+                if (bite == '\n')
+                {
+                    if (angle >= 25 && angle <= 155)
+                    {
+                        servo.write(angle);
+                        Serial.print(angle);
+                        Serial.println(" °");
+                    }
+                    else
+                        Serial.println("use values between 25 and 155");
+                }
+                else
+                    error();
+
+            default:
+                break;
+        }
+        Serial.println(state);
+    }
+}
